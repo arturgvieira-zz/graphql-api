d89095f3e23f9a561804f53911d78a939ab66435
Artur G Vieira <vieira.artur.g@gmail.com>
2018-03-01 23:06:51 -0800

    Update resolvers.js

--- server/resolvers.js
+++ server/resolvers.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Message = require('./model/message');
 
 mongoose.connect(
-    'mongodb://app:ev92ABCkRtxlCGQK@cluster0-shard-00-00-e7f4u.mongodb.net:27017,cluster0-shard-00-01-e7f4u.mongodb.net:27017,cluster0-shard-00-02-e7f4u.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin'
+    'connection string here'
 );
 
 var db = mongoose.connection;
